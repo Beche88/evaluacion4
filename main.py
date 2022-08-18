fd17c8d46893aff242cdf68b1f85359fc88b434a
Bruce <bruce.cg@outlook.com>
2022-08-17 20:49:09 -0400

    Evaluacion4.1

--- main.py
+++ main.py
@@ -1,17 +1,20 @@
 import  requests
 import json
+import urllib3
+import conf
+
+urllib3.disable_warnings()
 url = "http://127.0.0.1:58000"
 ###########################Obtencion de ticket
 
-data = {"password":"admin123!", "username":"admin"}
 cabecera ={"content-type":"application/json"}
-respuesta = requests.post(url+"/api/v1/ticket", json.dumps(data), headers=cabecera)
+respuesta = requests.post(url+"/api/v1/ticket", json.dumps(data), headers=cabecera, auth=(conf.username,conf.password),verify=False)
 token = respuesta.json()["response"]["serviceTicket"]
 
 
 ##################Inventario de equipo
 
 cabecera_inv = {"content-type":"application/json", "X-Auth-Token":token}
-inventario = requests.get(url+"/api/v1/network-device", headers=cabecera_inv)
+inventario = requests.get(url+"/api/v1/network-device", headers=cabecera_inv, auth=(conf.username,conf.password),verify=False)
 print(json.dumps(inventario.json(), indent=2))
 
